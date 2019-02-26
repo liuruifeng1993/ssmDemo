f463ed246937268c143b23a1586e926aa8e969d8
liuruifeng <994025680@qq.com>
2019-02-26 13:45:15 +0800

    第一次提交

---
+++ src/main/java/com/feng/dao/IUserDao.java
@@ -0,0 +1,9 @@
+package com.feng.dao;
+
+import com.feng.model.User;
+
+public interface IUserDao {
+
+    User selectUser(long id);
+
+}
---
+++ src/main/java/com/feng/service/IUserService.java
@@ -0,0 +1,9 @@
+package com.feng.service;
+
+import com.feng.model.User;
+
+public interface IUserService {
+
+    public User selectUser(long userId);
+
+}
